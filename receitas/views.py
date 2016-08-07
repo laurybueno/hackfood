cdfcd9f89c684d0585811347b548d967c2f1b7fd
Laury Bueno <laury@hacklab.com.br>
2016-08-07 19:14:49 -0300

    Views de API refatoradas para um padrão mais recente

--- receitas/views.py
+++ receitas/views.py
@@ -1,46 +1,55 @@
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from receitas.models import Receita, Ingrediente, Preparo
 from receitas.serializers import ReceitaSerializer
 
 
-class JSONResponse(HttpResponse):
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
 @csrf_exempt
+@api_view(['GET', 'POST'])
 def receitas_list(request):
 
     if request.method == 'GET':
-        receitas = Receita.objects.all()
+        receitas = Receita.objects.all()[:10]
         serializer = ReceitaSerializer(receitas, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         pass
-        # data = JSONParser().parse(request)
-        # serializer = SnippetSerializer(data=data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return JSONResponse(serializer.data, status=201)
-        # return JSONResponse(serializer.errors, status=400)
+        # receitas = []
+        # entrada = request.POST['ingrediente']
+        # ingredientes = []
+        #
+        # for ing in Ingrediente.objects.filter(nome__contains=entrada):
+        #     ingredientes.append(ing)
+        #
+        # for rec in Receita.objects.filter(ingrediente__in=ingredientes):
+        #     receitas.append(rec)
+        #
+        # serializer = ReceitaSerializer(data=receitas)
+        # resposta = {}
+        # # import pdb; pdb.set_trace()
+        # i = 0
+        # serializer.is_valid()
+        # for ser in serializer:
+        #     if ser.is_valid():
+        #         ser.save()
+        #         resposta[i] = ser
+        #     else:
+        #         return JSONResponse(serializer.errors, status=400)
+        #
+        # return JSONResponse(resposta.data, status=201)
 
 
+@api_view(['GET'])
 @csrf_exempt
 def receita_detalhe(request, pk):
 
     try:
         receita = Receita.objects.get(pk=pk)
     except Receita.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'GET':
-        serializer = ReceitaSerializer(receita)
-        return JSONResponse(serializer.data)
+    serializer = ReceitaSerializer(receita)
+    return Response(serializer.data, status=status.HTTP_200_OK)
