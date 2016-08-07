ba80050fd3f304daeddc76c6ce5ae2b314d1e10e
Laury Bueno <laury@hacklab.com.br>
2016-08-07 15:55:09 -0300

    Modelo de receitas melhorado para incluir ManytoMany

--- receitas/models.py
+++ receitas/models.py
@@ -3,15 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Receita(models.Model):
-    titulo = models.TextField()
-    ingrediente = models.ForeignKey('Ingrediente')
-    modo_preparo = models.ForeignKey('Preparo')
-
-
 class Ingrediente(models.Model):
     nome = models.TextField()
 
 
 class Preparo(models.Model):
     texto = models.TextField()
+
+
+class Receita(models.Model):
+    titulo = models.TextField()
+    ingrediente = models.ManyToManyField(Ingrediente, blank=True)
+    modo_preparo = models.ManyToManyField(Preparo, blank=True)
