17f8c373437e1bf5b2150b9e7dc5b0cbead7eec8
Laury Bueno <laury@hacklab.com.br>
2016-08-17 20:44:48 -0300

    Base de dsenvolvimento mudada para postgres

--- hackfood_back/settings.py
+++ hackfood_back/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'hackfood_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackfood',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
